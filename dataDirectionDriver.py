da6db0043a41af32faea17916cb3728cc7e73f4c
Leosly <Leonardonigro11@gmail.com>
2024-03-03 19:24:54 -0500

    commiting crimes

---
+++ dataDirectionDriver.py
@@ -0,0 +1,20 @@
+# as this data set grow I will likely want to reformat the data direction pattern and rules
+# already I want to split mains and futures into their own files giving futures a datePredicted parent and Mains a DateOfPrediction parent
+
+# will need a script to grab all files in data regardless of location and relocate them into a new schema
+
+"""
+Outline:
+for all elements in data of type json -> move to new schema
+if element = pattern 
+
+Can I turn this into a CI thing make it embeded into an action call, 
+
+okay Idea
+we use a node server to create an api to interact with this dataset
+Ive been wanting to make a node API anyway
+I'd like to run this script as a simple github action 
+
+I honestly dont want to make this in JS I stay an active JS hater and would rather eat rocks
+
+"""
